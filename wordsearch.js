02b165a92a02fb52a6fdd52b8024e7fbef746f30
Rajan Lail <rajanlail3993@gmail.com>
2023-06-16 11:34:10 -0700

    Format code

--- wordsearch.js
+++ wordsearch.js
@@ -1,35 +1,35 @@
-const wordSearch = (letters, word) => { 
-    if (letters.length === 0) {
-        return false;
-    }
-    
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-
-    const verticalJoin = transpose(letters).map(ls => ls.join(''))
-    for (h of verticalJoin) {
-        if (h.includes(word)) return true
-    }
-
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
     return false;
-}
-module.exports = wordSearch
+  }
+    
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-const transpose = function (matrix) {
-  const numRows = matrix.length; 
-  const numColums = matrix[0].length
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (h of verticalJoin) {
+    if (h.includes(word)) return true;
+  }
 
-const transposedMatrix = [];
-for (let i = 0; i < numColums; i++) {
-  const newRows = [];
+  return false;
+};
+module.exports = wordSearch;
 
-  for (let j = 0; j < numRows; j++){
-    newRows.push(matrix[j][i]);
-}
-  transposedMatrix.push(newRows)
-}
-   // Replace this code with your solution
+const transpose = function(matrix) {
+  const numRows = matrix.length;
+  const numColums = matrix[0].length;
+
+  const transposedMatrix = [];
+  for (let i = 0; i < numColums; i++) {
+    const newRows = [];
+
+    for (let j = 0; j < numRows; j++) {
+      newRows.push(matrix[j][i]);
+    }
+    transposedMatrix.push(newRows);
+  }
+  // Replace this code with your solution
   return transposedMatrix;
 };
